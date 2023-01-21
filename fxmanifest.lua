8264082f27427927074e273e9bd0ac49751e746c
SOLYMAN <hugogs0799@gmail.com>
2023-01-21 09:37:08 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'SOLYMAN™#6033 (HispanLifeRP)'
+description 'Discord: https://discord.gg/hispanliferp'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'css/style.css',
+    'js/script.js',
+    'img/logo.png',
+    'music/music.mp3',
+    'video/video.mp4'
+}
---
+++ js/script.js
@@ -0,0 +1,38 @@
+(function () {
+
+    var COUNT = 300;
+    var masthead = document.querySelector('.sky');
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    var width = masthead.clientWidth;
+    var height = masthead.clientHeight;
+    var i = 0;
+    var active = false;
+  
+    function onResize() {
+      width = masthead.clientWidth;
+      height = masthead.clientHeight;
+      canvas.width = width;
+      canvas.height = height;
+      ctx.fillStyle = '#FFF';
+  
+      var wasActive = active;
+      active = width > 600;
+
+    }
+  
+
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+  
+    onResize();
+    window.addEventListener('resize', onResize, false);
+  
+    masthead.appendChild(canvas);
+  })();
